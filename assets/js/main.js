caa271517d038f71be70813decb42320f881f0ab
redsonlopez <redsonlopez@gmail.com>
2025-11-20 21:12:49 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,12 @@
-
 const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
 fetch(url)
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (jsonBody) {
-    console.log(jsonBody)
-  })
-  .catch(function (error) {
-  console.error(error)
-  })
-  .finally(function () {
-    console.log('Requisição concluída!')
-  })
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch((error) => console.error(error))
 
 
+Teste:
+  hdsafvg
